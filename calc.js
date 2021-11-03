3ba42b736f52f3c639d206e0c556632b581a9984
LGMIGUEL12 <ramosvassquezluimiguel@gmail.com>
2021-11-03 14:57:15 -0600

    commit

--- calc.js
+++ calc.js
@@ -1,109 +1,26 @@
-var operancion1;
-var operacion2;
+var operacion1 = "";
+var operacion2 = "";
 var operar;
 
-function inicio() {
-    var resultado = document.getElementById("resultado");
-    var borrar = document.getElementById("borrar");
-    var listo = document.getElementById("listo");
-    var sumar = document.getElementById("sumar");
-    var restar = document.getElementById("restar");
-    var dividir = document.getElementById("dividir");
-    var multiplicar = document.getElementById("multiplicar");
-    var uno = document.getElementById("uno");
-    var dos = document.getElementById("dos");
-    var tres = document.getElementById("tres");
-    var cuatro = document.getElementById("cuatro");
-    var cinco = document.getElementById("cinco");
-    var seis = document.getElementById("seis");
-    var siete = document.getElementById("siete");
-    var ocho = document.getElementById("ocho");
-    var nueve = document.getElementById("nueve");
-    var cero = document.getElementById("cero");
+function insertar(event) {
+    var x = event.target.value;
+    var pantalla = document.getElementById("resultado").innerText;
 
-    uno.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "1"
-    }
-    dos.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "2"
-    }
-    tres.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "3"
-    }
-    cuatro.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "4"
-    }
-    cinco.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "5"
-    }
-    seis.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "6"
-    }
-    siete.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "7"
-    }
-    ocho.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "8"
-    }
-    nueve.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "9";
-    }
-    cero.onclick = function (e) {
-        resultado.textContent = resultado.textContent + "0";
-    }
-    borrar.onclick = function (e) {
-        borrartodo();
-    }
-    sumar.onclick = function (e) {
-        operancion1 = resultado.textContent;
-        operar = "+";
-        limpiar()
-    }
-    restar.onclick = function (e) {
-        operancion1 = resultado.textContent;
-        operar = "-";
-        limpiar()
-    }
-    dividir.onclick = function (e) {
-        operancion1 = resultado.textContent;
-        operar = "/";
-        limpiar()
-    }
-    multiplicar.onclick = function (e) {
-        operancion1 = resultado.textContent;
-        operar = "*";
-        limpiar()
-    }
-    listo.onclick = function (e) {
-        operacion2 = resultado.textContent;
-        resolver();
+    if ((parseFloat(x) >= "0" && !operar)) {
+        operacion1 = operacion1 + x;
+        document.getElementById("resultado").innerText = operacion1;
     }
 
-}
-
-function limpiar() {
-    resultado.textContent = "";
-}
-
-function borrartodo() {
-    resultado.textContent = "";
-    operancion1 = 0;
-    operacion2 = 0;
-    operar = "";
-}
-
-function resolver() {
-    var res = 0;
-    switch (operar) {
-        case "+": res = parseFloat(operancion1) + parseFloat(operacion2);
-            break;
-        case "-": res = parseFloat(operancion1) - parseFloat(operacion2);
-            break;
-        case "*": res = parseFloat(operancion1) * parseFloat(operacion2);
-            break;
-        case "/": res = parseFloat(operancion1) / parseFloat(operacion2);
-            break;
+    if (operar && parseFloat(x) >= "0") {
+        operacion2 = operacion2 + x;
+        document.getElementById("resultado").innerText = operacion2;
     }
-    borrartodo()
-    resultado.textContent = res;
+
+    if (!parseFloat(x) && !operacion2) {
+        operar = x;
+        document.getElementById("resultado").innerText = operacion1 + operar;
+    }
+    console.log("numero 1", operacion1)
+    console.log("numero 2", operacion2)
+    console.log("signo", operar)
 }
